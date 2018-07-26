9e5c33a901ef7db6568d0e4c8216557f3b147b80
Mahmoud Yusuf <mahmoud.yusuf@blink22.com>
2018-07-26 13:26:46 +0200

    Implement a drawer layout for todo list fragment

--- app/src/main/java/com/example/blink22/todo/TodoListFragment.java
+++ app/src/main/java/com/example/blink22/todo/TodoListFragment.java
@@ -3,10 +3,18 @@ package com.example.blink22.todo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,23 +28,68 @@ public class TodoListFragment extends Fragment implements ListView{
     @BindView(R.id.todo_list_recycler_view)
     RecyclerView mRecyclerView;
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
     TodoAdapter mAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View v = inflater.inflate(R.layout.fragment_todo_list, container, false);
+
         if(mTodoListPresenter == null){
             mTodoListPresenter = new TodoListPresenter(this);
         }
 
         ButterKnife.bind(this, v);
+
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+        ActionBar actionbar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         if(mAdapter == null){
             mAdapter = new TodoAdapter(mTodoListPresenter);
         }
         mRecyclerView.setAdapter(mAdapter);
+
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        //open new activity or something else;
+                        return true;
+                    }
+                }
+        );
         return v;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
 }
